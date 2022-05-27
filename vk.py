5d84a5e8ca22203800ab600effdfe824add50cb9
Roman Volodin <roman.volodin@gmail.com>
2022-05-27 23:13:03 +0300

    Echobot for VK

--- vk.py
+++ vk.py
@@ -1,19 +1,26 @@
+import random
+
 import vk_api
-from vk_api.longpoll import VkLongPoll, VkEventType
 from environs import Env
+from vk_api.longpoll import VkEventType, VkLongPoll
 
-env = Env()
-env.read_env()
 
-vk_session = vk_api.VkApi(token=env.str("VK_ACCESS_KEY"))
+def echo(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        message=event.text,
+        random_id=random.randint(1, 1000),
+    )
 
-longpoll = VkLongPoll(vk_session)
 
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW:
-        print("Новое сообщение:")
-        if event.to_me:
-            print("Для меня от: ", event.user_id)
-        else:
-            print("От меня для: ", event.user_id)
-        print("Текст:", event.text)
+if __name__ == "__main__":
+    env = Env()
+    env.read_env()
+
+    vk_session = vk_api.VkApi(token=env.str("VK_ACCESS_KEY"))
+    api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            echo(event, api)
